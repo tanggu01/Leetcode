05b605832b8423ee6b80cae4b7ac1d919ae7405f
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-16 20:25:40 +0900

    Time: 0 ms (100.00%), Space: 43.6 MB (48.71%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        //O(n2)
+        /* O(n2)
         int row = mat.length;
         int col = mat[0].length;
         if (row * col != r * c) 
@@ -18,18 +18,19 @@ class Solution {
             }
         }
         return newMat;
-    } 
+    } */
 
-    // O(n)
-    //     if (mat.length * mat[0].length != r * c) {
-    //         return mat;
-    //     }
-    //     int[][] newMat = new int[r][c];
-    //     int row = mat.length;
-    //     int col = mat[0].length; 
-    //     for (int i = 0; i < row * col; i++) {
-    //         newMat[i/c][i%c] = mat[i/col][i%col];
-    //     }
-    //     return newMat;
-    // }
+
+    //O(n)
+        if (mat.length * mat[0].length != r * c) {
+            return mat;
+        }
+        int[][] newMat = new int[r][c];
+        int row = mat.length;
+        int col = mat[0].length; 
+        for (int i = 0; i < row * col; i++) {
+            newMat[i/c][i%c] = mat[i/col][i%col];
+        }
+        return newMat;
+    }
 }
