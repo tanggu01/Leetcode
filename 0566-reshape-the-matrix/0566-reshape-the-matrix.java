271302252c889014224f3c0ca74aa6e3c3729343
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-16 20:24:39 +0900

    Time: 0 ms (100.00%), Space: 43.5 MB (48.71%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -1,22 +1,35 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        if (mat.length * mat[0].length != r * c) 
+        //O(n2)
+        int row = mat.length;
+        int col = mat[0].length;
+        if (row * col != r * c) 
             return mat;
-        
         int[][] newMat = new int[r][c];
-        ArrayList<Integer> list = new ArrayList<>();
-        int k = 0;
-        for (int[] row : mat) {
-            for (int num: row) {
-                list.add(num);
-            }
-        }
-        
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                newMat[i][j] = list.get(k++);
+        int newRow = 0, newCol = 0;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                newMat[newRow][newCol] = mat[i][j];
+                newCol++;
+                if (newCol == c) {
+                    newRow++;
+                    newCol = 0;
+                }
             }
         }
         return newMat;
-    }
+    } 
+
+    // O(n)
+    //     if (mat.length * mat[0].length != r * c) {
+    //         return mat;
+    //     }
+    //     int[][] newMat = new int[r][c];
+    //     int row = mat.length;
+    //     int col = mat[0].length; 
+    //     for (int i = 0; i < row * col; i++) {
+    //         newMat[i/c][i%c] = mat[i/col][i%col];
+    //     }
+    //     return newMat;
+    // }
 }
