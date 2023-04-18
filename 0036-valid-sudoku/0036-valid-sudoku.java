44d32ebc63deee1bdb294dcbb8b738e557d8ca2b
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-18 16:37:16 +0900

    Time: 2 ms (85.81%), Space: 42.4 MB (81.23%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -27,26 +27,3 @@ class Solution { //2ms
         return true;
     }
 }
-
-/* 3ms
-class Solution { //1. '.'으로 비교 ; 2. set은 put아니고 add임
-    public boolean isValidSudoku(char[][] board) {
-        for (int i = 0; i < 9; i++) { 
-            Set<Character> colSet = new HashSet<>();
-            Set<Character> rowSet = new HashSet<>();
-            Set<Character> subSet = new HashSet<>();
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] != '.' && !colSet.add(board[i][j])) //col 
-                return false;
-                if (board[j][i] != '.' && !rowSet.add(board[j][i])) //row 
-                return false;
-                int rowIndex = 3 * (i/3); //0,0,0 3,3,3, 6,6,6
-                int colIndex = 3 * (i%3); //0,3,6 0,3,6 0,3,6
-                if (board[rowIndex+j/3][colIndex+j%3] != '.' && !subSet.add(board[rowIndex + j/3][colIndex + j%3])) 
-                    return false;
-            }
-        }
-        return true;
-    }
-}
-*/
