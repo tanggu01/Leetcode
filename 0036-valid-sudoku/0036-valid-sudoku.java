a694523cbd789c5af91e853c29300aeffd8a375c
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-18 16:26:49 +0900

    Time: 2 ms (85.81%), Space: 43.2 MB (25.19%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,32 @@
+class Solution { //1. '.'으로 비교 ; 2. set은 put아니고 add임
+    public boolean isValidSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) { //col 
+            Set<Character> colSet = new HashSet<>();
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.' && !colSet.add(board[i][j])) {
+                return false;
+                }
+            }
+        }
+        for (int i = 0; i < board.length; i++) { //row 
+            Set<Character> rowSet = new HashSet<>();
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[j][i] != '.' && !rowSet.add(board[j][i])) {
+                return false;
+                }
+            }
+        }
+
+        for (int i = 0 ; i < board.length; i++) { //박스 하나 * 9번
+            Set<Character> subSet = new HashSet<>();
+            for (int j = 0; j < 9; j++ ) {//박스 안 9번 
+                int rowIndex = 3 * (i/3); //0,0,0 3,3,3, 6,6,6
+                int colIndex = 3 * (i%3); //0,3,6 0,3,6 0,3,6
+                if (board[rowIndex+j/3][colIndex+j%3] != '.' && !subSet.add(board[rowIndex + j/3][colIndex + j%3])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
