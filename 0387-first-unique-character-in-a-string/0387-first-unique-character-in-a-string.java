ee78ad8c828509b685deb1c5e1cabbd706199783
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-21 17:57:54 +0900

    Time: 1 ms (100.00%), Space: 42.7 MB (92.24%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -11,30 +11,3 @@ class Solution {
         return ans;
     }
 }
-
-// class Solution {
-//     public int firstUniqChar(String s) {
-//     for(char c : s.toCharArray()){
-//         int index = s.indexOf(c);
-//         int lastIndex = s.lastIndexOf(c);
-//         if(index == lastIndex)
-//             return index;
-//     }
-//         return -1;
-//     }
-// }
-
-// class Solution {
-//     public int firstUniqChar(String s) {
-//         Map<Character, Integer> map = new HashMap<>();
-//         for (int i = 0; i < s.length(); i++) {
-//             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1); //char, frequency
-//         }
-//         for (int i = 0; i < s.length(); i++) {
-//             if (map.get(s.charAt(i)) == 1) {
-//                 return i;
-//             }
-//         }
-//         return -1;
-//     }
-// }
