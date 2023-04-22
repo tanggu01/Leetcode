07dd84c80e63d5c5eb56bcefb52ae6858030e91b
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-23 01:00:13 +0900

    Time: 1 ms (80.39%), Space: 43.1 MB (93.83%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -15,7 +15,7 @@ class Solution {
         ListNode curr = dummy;
         while (curr.next != null) { //curr.next = head (1)
             if (curr.next.val == val) { // 1 == 1
-                curr.next = curr.next.next; // curr.next = 2 increment 대신 curr.next가 알아서 앞으로 옴
+                curr.next = curr.next.next; // curr.next = 2
             } else {
                 curr = curr.next; // curr = 0 -> 2 
             }
