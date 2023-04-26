8fd905cfe143bff9f95d6d72a590673db60c8c57
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-04-26 22:42:27 +0900

    Time: 2 ms (88.57%), Space: 40.8 MB (37.65%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,6 +1,5 @@
 class Solution {
     public boolean isValid(String s) {
-        if (s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
         for (char c : s.toCharArray()) {
             if (c == '(') stack.push(')');
