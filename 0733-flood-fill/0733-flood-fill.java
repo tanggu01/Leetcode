c5115be6c816a5d9ad74d9c14bae613298bc1095
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-05-18 22:23:33 +0900

    Time: 1 ms (43.38%), Space: 43.6 MB (46.14%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,17 +1,13 @@
 class Solution {
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-
         boolean[][] visited = new boolean[image.length][image[0].length];
         dfs(image, sr, sc, color, image[sr][sc], visited);
         return image;
     }
 
     public void dfs(int[][] image, int sr, int sc, int newColor, int baseColor, boolean[][] visited) { 
-        int rl = image.length;
-        int cl = image[0].length;
-
         // 1. base condition 
-        if(sc < 0 || sc >= cl || sr < 0 || sr >= rl || image[sr][sc] != baseColor || visited[sr][sc]) { //base case
+        if(sc < 0 || sc >= image[0].length || sr < 0 || sr >= image.length || image[sr][sc] != baseColor || visited[sr][sc]){ 
             return;
         }
         //mark the node as visited
