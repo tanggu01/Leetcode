de444eb7923e0f65462f910f69730ec001fa9d8e
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-06-27 23:17:54 +0900

    Time: 1 ms (84.74%), Space: 41.6 MB (45.71%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -1,22 +1,37 @@
 class Solution {
-    public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        for (int i = Math.min(len1, len2); i >= 1; i--) {
-            if (valid(str1, str2, i)) {
-                return str1.substring(0,i);
-            }
-        }
-        return "";
+    public int gcd(int a, int b) {
+        int r = a % b;
+        if (r == 0) return b;
+        else return gcd(b, r); 
     }
-
-    public boolean valid(String str1, String str2, int k) {
-        int len1 = str1.length(), len2 = str2.length();
-        if (len1 % k > 0 || len2 % k > 0) {
-            return false;
-        } else {
-            String base = str1.substring(0,k);
-            //다 치환했을시 ""가 되어야 gcd
-            return str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty(); 
-        }
+    
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) return "";
+        
+        // Get the GCD of the two lengths.
+        int gcdLength = gcd(str1.length(), str2.length());
+        return str1.substring(0, gcdLength);
     }
 }
+
+
+// class Solution {
+//     public int gcd(int x, int y) {
+//         if (y == 0) {
+//             return x;
+//         } else {
+//             return gcd(y, x % y);
+//         }    
+//     }
+    
+//     public String gcdOfStrings(String str1, String str2) {
+//         // Check if they have non-zero GCD string.
+//         if (!(str1 + str2).equals(str2 + str1)) {
+//             return "";
+//         }
+        
+//         // Get the GCD of the two lengths.
+//         int gcdLength = gcd(str1.length(), str2.length());
+//         return str1.substring(0, gcdLength);
+//     }
+// }
