8af93e1c7f4ef681d061a0f71e84765a7468beb3
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-10-25 20:54:55 +0900

    Time: 0 ms (100%), Space: 40.5 MB (75.81%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -4,24 +4,13 @@ class Solution {
         int l2 = word2.length();
         int p1 = 0;
         int p2 = 0;
-        int n = Math.min(l1,l2);
-        int i = 0 ;
         StringBuilder str = new StringBuilder();
 
-        while (p1 < n || p2 < n) {
-            if (i % 2 == 0) {
-                str.append(word1.charAt(p1));
-                p1++;
-            } else {
-                str.append(word2.charAt(p2));
-                p2++;
-            }
-            i++;
-        }
-        if (p1 < l1){
-            str.append(word1.substring(p1, l1));
-        } else if (p2 < l2){
-            str.append(word2.substring(p2, l2));
+        while (p1 < l1 || p2 < l2) {
+            if (p1 < l1) 
+                str.append(word1.charAt(p1++));
+            if (p2 < l2)
+                str.append(word2.charAt(p2++));
         }
         return str.toString();
     }
