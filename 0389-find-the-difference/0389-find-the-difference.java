0288116dd7f71aef260e934ab76c01df635a4f86
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-10-26 00:39:16 +0900

    Time: 1 ms (100%), Space: 40.6 MB (56.21%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,28 +1,8 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        Map<Character, Integer> map = new HashMap<>();
-        int idx = 0;
-
-        for (int i = 0; i < s.length(); i++) {
-            map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            System.out.println("entry.getKey()  + entry.getValue() = " + entry.getKey() + " / /  " + entry.getValue());
-        }
-        for (int i = 0; i < t.length(); i++) {
-            // 없거나 ()null), 0이었거나 (그러면 -1 이 아니라 이다음에 -1)
-            if(map.get(t.charAt(i)) == null || map.put(t.charAt(i), map.get(t.charAt(i)) - 1) <= 0) {
-                System.out.println(idx);
-                idx = i;
-                break;
-            }
-        }
-        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
-            System.out.println("entry.getKey()  + entry.getValue() = " + entry.getKey() + " / /  " + entry.getValue());
-        }
-        System.out.println(map);
-        System.out.println(idx);
-
-        return t.charAt(idx);
+        char c = 0;
+        for(char cs : s.toCharArray()) c ^= cs;
+        for(char ct : t.toCharArray()) c ^= ct;
+        return c;
     }
 }
