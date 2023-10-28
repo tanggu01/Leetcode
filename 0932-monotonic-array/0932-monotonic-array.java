5ccb7922d8f132e76e8420c02360ab5734476394
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-10-28 22:30:46 +0900

    Time: 2 ms (57.95%), Space: 56.3 MB (22.62%) - LeetHub

--- 0932-monotonic-array/0932-monotonic-array.java
+++ 0932-monotonic-array/0932-monotonic-array.java
@@ -1,28 +1,17 @@
 class Solution {
     public boolean isMonotonic(int[] nums) {
-        //min, max 구해서 하면 asc, desc 모름 -> loop 으로 찾기
-        int diff = 0;
-        for (int i = 1; i < nums.length; i++) {
-            if (nums[i] - nums[i-1] != 0) {
-                diff = nums[i] - nums[i-1];
-                break;
-            }
-        }
+        boolean inc = true;
+        boolean dec = true;
 
-        if (diff == 0) {
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] - nums[i-1] != 0) return false;
-            }            
+        for (int i = 1; i < nums.length; i++) {
+            if (!inc && !dec) {
+                return false;
+            } else if (nums[i] > nums[i-1]) {
+                dec = false;
+            } else if (nums[i] < nums[i-1]) {
+                inc = false;
+            } 
         }
-        else if (diff < 0) {
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] - nums[i-1] > 0) return false;
-            }
-        } else if (diff > 0) {
-            for (int i = 1; i < nums.length; i++) {
-                if (nums[i] - nums[i-1] < 0) return false;
-            }
-        }
-        return true;
+        return inc || dec;
     }
 }
