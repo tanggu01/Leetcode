7404ee7347a5e0ae160d49bf13a693c481d9ca46
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-11-04 13:29:56 +0900

    Time: 1 ms (25.53%), Space: 40.1 MB (77.86%) - LeetHub

---
+++ 1119-robot-bounded-in-circle/1119-robot-bounded-in-circle.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isRobotBounded(String instructions) {
+        //cycle: return to (0,0)
+        int x = 0;
+        int y = 0;
+        int[][] dir = {{0, 1}, {-1, 0}, {0, -1}, {1, 0}};
+
+        int currDir = 0; // north
+
+        for (int i = 0; i < 4; i++) {
+            for (char c : instructions.toCharArray()) {
+                if (c == 'G') {
+                    x += dir[currDir][0];
+                    y += dir[currDir][1];
+                }
+                else if (c == 'L') { 
+                    // currDir += 1;
+                    currDir = (currDir + 1) % 4;
+                }
+                else if (c == 'R') {
+                    // currDir -= 1;
+                    currDir = (currDir + 3) % 4; //same as -1
+                }
+            }
+            if (x == 0 && y == 0) return true;
+        }
+        return (x == 0 && y == 0);
+    }
+}
