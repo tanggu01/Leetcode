e8218172307c3b609307b7583371d3d3d285c299
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-11-05 17:25:06 +0900

    Time: 8 ms (98.48%), Space: 44.2 MB (43.91%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -2,17 +2,16 @@ class Solution {
     public int findContentChildren(int[] g, int[] s) {
         Arrays.sort(g);
         Arrays.sort(s);
-
         int i = 0;
         int j = 0;
         int maxChild = 0;
 
         while (i < g.length && j < s.length) {
-            if (g[i] <= s[j]) { //can feed
+            if (g[i] <= s[j]) { 
                 i++;
                 j++;
                 maxChild++;
-            } else if (g[i] > s[j]) {
+            } else {
                 j++;
             }
         }
