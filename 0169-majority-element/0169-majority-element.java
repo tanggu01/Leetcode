28ca86e96ef8d73aa7c3b35957532e966c760016
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-11-05 15:09:41 +0900

    Time: 1 ms (99.9%), Space: 48.2 MB (63.93%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length / 2];
+        int maj = nums[0];
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+               maj = nums[i];
+            } 
+            if (maj == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return maj;
     }
 }
