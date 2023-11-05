10259c010de5ee2f2014ede44b0caab3c25f6ce5
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-11-05 17:58:57 +0900

    Time: 3 ms (78.26%), Space: 43.5 MB (29.41%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.java
+++ 0414-third-maximum-number/0414-third-maximum-number.java
@@ -4,14 +4,12 @@ class Solution {
 
         int res = nums[nums.length - 1];
         int count = 1;
-        for (int i = nums.length - 1; i > 0; i--) {
+        for (int i = nums.length - 1; i > 0 && count < 3; i--) {
             if (nums[i-1] != nums[i]) { 
                res = nums[i-1];
                count++;
             }
-            if (count == 3) return res;
         }
-        if (count < 3) return nums[nums.length - 1];
-        return res;
+        return count == 3 ? res : nums[nums.length - 1];
     }
 }
