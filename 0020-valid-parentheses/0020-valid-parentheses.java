c293fe94cab78cdebee5bf51867a9d9816ac79e0
KIM TAEHYUN <93398875+tanggu01@users.noreply.github.com>
2023-11-07 20:10:41 +0900

    Time: 2 ms (83.16%), Space: 40.2 MB (77.66%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,12 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for (char c : s.toCharArray()) {
-            if (c == '(') stack.push(')');
-            else if (c == '{') stack.push('}');
-            else if (c == '[') stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+        char[] arr = s.toCharArray(); 
+        if (arr[0] != '(' && arr[0] != '[' && arr[0] != '{') return false;
+
+        for (char c: arr) {
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (!stack.isEmpty()){ 
+                if (c == ')' && stack.peek() != '(') return false;
+                if (c == ']' && stack.peek() != '[') return false;
+                if (c == '}' && stack.peek() != '{') return false;
+                else stack.pop();
+            } else {
+                return false;
+            }
         }
-    return stack.isEmpty();
+        return stack.isEmpty();
     }
 }
